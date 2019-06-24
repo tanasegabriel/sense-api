1eccca6ab935adc848d7eea13f077b80f5c6edde
tanasegabriel <tanase.gabriel91@gmail.com>
2019-06-24 22:03:36 +0100

    Add led_matrix tests

--- tests/test_senseapi.py
+++ tests/test_senseapi.py
@@ -9,4 +9,4 @@ atexit.register(sense_emulator.terminate)
 gunicorn = subprocess.Popen(["gunicorn", "-b", "127.0.0.1:8000", "senseapi", "emulation"])
 atexit.register(gunicorn.terminate)
 
-pytest.main(["tests"])
+pytest.main(["tests", "tavern_senseapi_v1"])
